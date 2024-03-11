63002925317149b87318d2c4e1017994909e05d6
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-03-11 22:25:47 +0200

    File

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-"""
-Define square class
-"""
+"""Define Square Class"""
 class square():
-"""Class Square"""
+    """ Square Class"""
     width = 0
+    height = 0
 
+    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def Permiter_Of_My_Square(self):
-        return (self.width * 2) + (self.width * 2)
+    def PermiterOfMySquare(self):
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "{}".format(self.width)
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(width=12)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.Permiter_Of_My_Square())  # corrected method name
+    print(s.PermiterOfMySquare())
 
