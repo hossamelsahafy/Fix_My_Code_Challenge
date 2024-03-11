a876330558026c1a7abaee9ec849550d6157bfed
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-03-11 22:29:02 +0200

    File

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Define Square Class"""
+
+
 class square():
     """ Square Class"""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -20,10 +21,10 @@ class square():
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
 
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
-
